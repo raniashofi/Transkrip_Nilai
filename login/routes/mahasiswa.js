4fabf8d676922e8c8dc184ed9ac2d09eace1a26b
ryanashoffan <mryanashoffan05@gmail.com>
2024-05-21 16:10:03 +0700

    lihat profil fix

--- login/routes/mahasiswa.js
+++ login/routes/mahasiswa.js
@@ -1,20 +1,10 @@
 import express from "express";
 import { verifyToken } from "../middleware/validtokenMiddleware.js";
-import { checkUserLoggedIn } from "../controllers/auth.js"; // Pastikan Anda mengimpor fungsi checkUserLoggedIn
+import { getProfileMhs } from "../controllers/auth.js"; // Pastikan Anda mengimpor fungsi checkUserLoggedIn
 
 const router = express.Router();
 
-router.get("/home", verifyToken('mahasiswa'), async function (req, res) {
-  try {
-    const { user } = checkUserLoggedIn(req, res);
-    if (!user) {
-      return res.redirect('/login');
-    }
-    res.render("home", { user }); // Assuming your view file is named 'home.ejs'
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+router.get('/home', verifyToken('mahasiswa'), getProfileMhs);
+
 
 export default router;
