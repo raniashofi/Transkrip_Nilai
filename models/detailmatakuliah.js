9ecd030770235d07e93090dc1e480bc87c3eb41e
raniashofi <raniashofim13@gmail.com>
2024-06-02 23:57:56 +0700

    input nilai (belum fix)

--- models/detailmatakuliah.js
+++ models/detailmatakuliah.js
@@ -1,35 +1,44 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class DetailMataKuliah extends Model {
-    static associate(models) {
-      DetailMataKuliah.belongsTo(models.User, { foreignKey: 'nim_nip' });
-      DetailMataKuliah.belongsTo(models.MataKuliah, { foreignKey: 'kode_matkul' });
-    }
-  }
-  DetailMataKuliah.init({
-    nim_nip: { // Add this field
-      type: DataTypes.STRING,
-      references: {
-        model: 'Users', // name of Target model
-        key: 'nim_nip', // key in Target model that we're referencing
-      },
-    },
-    kode_matkul: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'MataKuliahs',
-        key: 'kode_matkul',
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'DetailMataKuliah',
-    tableName: 'DetailMataKuliahs',
-    timestamps: true, // Add this line if timestamps are needed
-    createdAt: 'createdAt', // Rename createdAt column if necessary
-    updatedAt: 'updatedAt', // Rename updatedAt column if necessary
-  });
-  return DetailMataKuliah;
+	class DetailMataKuliah extends Model {
+		static associate(models) {
+			DetailMataKuliah.belongsTo(models.User, { foreignKey: 'nim_nip' });
+			DetailMataKuliah.belongsTo(models.MataKuliah, { foreignKey: 'kode_matkul' });
+		}
+	}
+	DetailMataKuliah.init(
+		{
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			nim_nip: {
+				type: DataTypes.STRING,
+				references: {
+					model: 'Users',
+					key: 'nim_nip',
+				},
+			},
+			kode_matkul: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				references: {
+					model: 'MataKuliahs',
+					key: 'kode_matkul',
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: 'DetailMataKuliah',
+			tableName: 'DetailMataKuliahs',
+			timestamps: true,
+			createdAt: 'createdAt',
+			updatedAt: 'updatedAt',
+		}
+	);
+	return DetailMataKuliah;
 };
--- routes/dosen.js
+++ routes/dosen.js
@@ -1,53 +1,36 @@
-const express = require("express");
-const verifyToken = require("../middleware/validtokenMiddleware.js");
-const {
-  getUser,
-  getProfileDosen,
-  changePassword,
-} = require("../controllers/auth.js");
-const { getNilai, inputNilaiMahasiswa, saveNilaiMahasiswa } = require("../controllers/dosenController.js");
+const express = require('express');
+const verifyToken = require('../middleware/validtokenMiddleware.js');
+const { getUser, getProfileDosen, changePassword } = require('../controllers/auth.js');
+const { getNilai, updateNilai } = require('../controllers/dosenController.js');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.redirect("/dashboard");
+router.get('/', (req, res) => {
+	res.redirect('/dashboard');
 });
 
-router.get("/dashboard", verifyToken("dosen"), async (req, res) => {
-  try {
-    const user = await getUser(req, res);
-    res.render("dosen/dashboard", { user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+router.get('/dashboard', verifyToken('dosen'), async (req, res) => {
+	try {
+		const user = await getUser(req, res);
+		res.render('dosen/dashboard', { user });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
 
-router.get("/profile", verifyToken("dosen"), (req, res, next) => {
-    next();
-  },
-  getProfileDosen
-);
+router.get('/profile', verifyToken('dosen'), getProfileDosen);
 
-router.get(
-  "/profile/changePass",
-  verifyToken("dosen"),
-  async function (req, res) {
-    const user = await getUser(req, res);
-    res.render("dosen/changePass", { user });
-  }
-);
-
-router.post("/changePass", verifyToken("dosen"), async (req, res) => {
-  await changePassword(req, res);
+router.get('/profile/changePass', verifyToken('dosen'), async function (req, res) {
+	const user = await getUser(req, res);
+	res.render('dosen/changePass', { user });
 });
 
-router.get("/nilai", verifyToken("dosen"), (req, res, next) => {
-    next();
-  },
-  getNilai
-);
+router.post('/changePass', verifyToken('dosen'), async (req, res) => {
+	await changePassword(req, res);
+});
 
-
+router.get('/nilai', verifyToken('dosen'), getNilai);
+router.post('/nilai/:nim_nip', verifyToken('dosen'), updateNilai);
 
 module.exports = router;
